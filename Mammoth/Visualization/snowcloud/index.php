acd0a7a261888b7db5f2cd3544e7b4b71904e62d
rob <rkrattli@gmail.com>
2013-12-06 13:37:42 -0500

    updated visualization files

--- Mammoth/Visualization/snowcloud/index.php
+++ Mammoth/Visualization/snowcloud/index.php
@@ -1,9 +1,7 @@
 <?php
-
-/* Main Processed Interface. */
-include("connect.php");  //connects once to the database
-include("functions.php"); //functions for creating plots, csv, and table.
-include("template_header.php");  //includes header html and javascript for date picker and High Charts.
+include("connect.php"); 
+include("functions.php"); 
+include("template_header.php");  
 echo'
     <form id="snowcloudinterface" name="snowcloudinterface" method="post" action="' . $_SERVER['PHP_SELF'] . '">
 	<table id="index">
@@ -30,14 +28,22 @@ while ($row = mysql_fetch_array($result)) {
         echo '<tr><td><input type="checkbox" name="towers[]" value="' . $towerID . '" id="tower' . $towerID . '"';
         if (isset($_POST["towers"]) && in_array($towerID, $_POST["towers"]))
             echo ' checked="checked"';
-        echo'/><label for="tower' . $towerID . '">Tower ' . $towerID . '</label></td><td>' . $longitude . '</td><td>' . $latitude . '</td>';
+
+		if($_SERVER['REQUEST_METHOD'] != "POST")
+        echo'checked><label for="tower' . $towerID . '">Tower ' . $towerID . '</label></td><td>' . $longitude . '</td><td>' . $latitude . '</td>';
+			else echo'/><label for="tower' . $towerID . '">Tower ' . $towerID . '</label></td><td>' . $longitude . '</td><td>' . $latitude . '</td>';
+			
         $towercount[$towerID] = 1;
         $count++;
 
         echo '<td style="padding:20px;" rowspan="3"><label for="sensor0">Snow Depth:</label><input type=checkbox name="sensors[]" value="0" id="sensor0"';
         if (isset($_POST["sensors"]) && in_array(0, $_POST["sensors"]))
             echo ' checked="checked"';
-        echo'/> <label for="sensor1">Sky PAR:</label><input type=checkbox name="sensors[]" value="1" id="sensor1"';
+
+		if($_SERVER['REQUEST_METHOD'] != "POST")
+        echo'checked> <label for="sensor1">Sky PAR:</label><input type=checkbox name="sensors[]" value="1" id="sensor1"';
+			else echo'/> <label for="sensor1">Sky PAR:</label><input type=checkbox name="sensors[]" value="1" id="sensor1"';
+
         if (isset($_POST["sensors"]) && in_array(1, $_POST["sensors"]))
             echo ' checked="checked"';
         echo'/> <label for="sensor5">Air Temp:</label><input type=checkbox name="sensors[]" value="5" id="sensor5"';
@@ -71,13 +77,20 @@ while ($row = mysql_fetch_array($result)) {
 echo '<tr><td colspan="4">Start time: <input type="text" name="starttime" id="starttime"';
 if (isset($_POST["starttime"]))
     echo ' value="' . $_POST["starttime"] . '"';
+else echo ' value="2013-01-01 00:00:00"';
 echo' /></td></tr><tr><td colspan="4">End time: <input type="text" name="endtime" id="endtime"';
 if (isset($_POST["endtime"]))
     echo ' value="' . $_POST["endtime"] . '"';
+else echo ' value="' . date('Y-m-d H:i:s') . '"';
+
 echo' /></td></tr><tr><td colspan="4"><label for="plot">Plot:</label><input type=checkbox name="plot" value="yes" id="plot"';
 if ($_POST["plot"] == 'yes')
     echo ' checked="checked"';
-echo '/> <label for="csv">CSV:</label><input type=checkbox name="csv" value="yes" id="csv"';
+
+if($_SERVER['REQUEST_METHOD'] != "POST")
+echo 'checked> <label for="csv">CSV:</label><input type=checkbox name="csv" value="yes" id="csv"';
+else echo'/> <label for="csv">CSV:</label><input type=checkbox name="csv" value="yes" id="csv"';
+
 if ($_POST["csv"] == 'yes')
     echo ' checked="checked"';
 echo'/> <label for="table">Table:</label><input type=checkbox name="table" value="yes" id="table"';
@@ -85,7 +98,12 @@ if ($_POST["table"] == 'yes')
     echo ' checked="checked"';
 echo'/></td></tr><tr><td colspan="4"><input type="submit" name="submitted" value="Submit" /><a style="float:right;" href="' . $_SERVER['PHP_SELF'] . '">Reset Fields</a></td></tr></table></form>';
 ?>
+
 <?php
+//Automatically query the server if a query hasn't already been made
+if($_SERVER['REQUEST_METHOD'] != "POST") {
+ echo '<script>document.snowcloudinterface.submit();</script>';
+}
 
 //Draw Graphs
 if ($_POST["plot"] == 'yes') {
